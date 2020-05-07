c576e2fcb43990fb9e761e92269c09ecb7ed626b
Roy <roygufo@gmail.com>
2020-05-07 16:11:34 -0500

    Add email and token fields to user object

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -7,19 +7,27 @@ String userToJson(User data) => json.encode(data.toJson());
 class User {
     String name;
     String id;
+    String email;
+    String token;
 
     User({
         this.name,
         this.id,
+        this.email,
+        this.token,
     });
 
     factory User.fromJson(Map<String, dynamic> json) => User(
         name: json["name"],
         id: json["id"],
+        email: json["email"],
+        token: json["token"],
     );
 
     Map<String, dynamic> toJson() => {
         "name": name,
         "id": id,
+        "email": email,
+        "token": token,
     };
 }
